02807f0d587aff370932339210db1fb95d2af98f
felixg97 <felixgerschner@felix-mac.local>
2022-05-05 09:27:02 +0200

    added clustering folder

---
+++ clustering/kmeans/kmeans_simple.py
@@ -0,0 +1,28 @@
+
+# Required packages
+import numpy as np
+
+from sklearn import datasets
+
+iris = datasets.load_iris()
+
+print(iris)
+
+# define the distance measure / norm to use
+def eucledian_distance(x_1, x_2):
+    distance = 0
+
+    return distance
+
+
+
+# k-Means cluster algorithm
+def k_means(num_k, initialization, iterations, distance_measure):
+
+    
+
+    pass
+
+
+# Perform clustering
+print(iris)

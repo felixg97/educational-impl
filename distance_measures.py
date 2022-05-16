ad3b8c23a44f8878be6cefd5bfdffeedacb9a567
felixg97 <felixgerschner@felix-mac.local>
2022-05-16 21:45:54 +0200

    added distance_measure changes

--- distance_measures.py
+++ distance_measures.py
@@ -1,24 +1,9 @@
-import math
+import numpy as np
 
 
-def eucledian_distance(x_1, x_2):
-    sum_squared_differences = 0
-
-    # summation of the squared differences
-    for i in range(len(x_1)):
-        sum_squared_differences += (x_1[i] - x_2[i])**2
-
-    # taking the square root of the sum of squared differences
-    distance = math.sqrt(sum_squared_differences)
-
-    return distance
+def dist_eucledian(x1, x2):
+    return np.sqrt(np.sum(np.square(np.subtract(x1, x2))))
 
 
-def manhattan_distance(x_1, x_2):
-    sum_absolute_differences = 0
-
-    # summation of the absolute differences
-    for i in range(len(x_1)):
-        sum_absolute_differences += abs(x_1[i] - x_2[i])
-
-    return sum_absolute_differences 
+def manhattan_distance(x1, x2):
+    return np.sum(np.absolute(np.subtract(x1, x2)))
